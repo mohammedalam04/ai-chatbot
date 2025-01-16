dad6df10904da484fcc6f42626f5044918a870ef
Mohammed Alam <mohammed.alam1@ontariotechu.net>
2025-01-15 19:19:19 -0500

    initial

--- learner.cpp
+++ learner.cpp
@@ -9,5 +9,23 @@ using namespace::std;
 void learner(string inputPhrase) {
   string storePhrase = inputPhrase;
 
-  
+  std::cout << "Not found! What should the response be?" << storePhrase << " : ";
+  std::string learningResponse;
+  getline(std::cin, learningResponse);
+  std::cout << learningResponse << endl;
+
+  // open file for writing parameters indicate that you will 
+  // APPEND to the end of the file
+  std::ofstream saveBrain;
+
+  saveBrain.open("brain/brain.txt", std::ios::out | std::ios::app);
+  if(!saveBrain.is_open()) {
+    std::cout << "Error opening file." << endl;
+  } else {
+    saveBrain <<endl <<storePhrase << "|" << learningResponse;
+    saveBrain.close();
+  }
+
+  return;
+
 }
